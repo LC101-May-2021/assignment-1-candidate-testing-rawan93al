a3c6e6bcaaf485489846564773a2d6463d34a30c
Rawan Almasalmeh <>
2021-05-24 15:34:54 +0000

    Asked for name and displayed greeting

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName=" ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
 let correctAnswer;
@@ -15,11 +15,15 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+ candidateName=input.question("Enter your name: ");
+ 
+ return (candidateName);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
+
+
 
 
 }
@@ -38,7 +42,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+console.log("Hello"+" "+candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
