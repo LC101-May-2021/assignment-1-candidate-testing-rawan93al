f48f038423cf24df2c835e5b686e2b440e6faab1
Rawan Almasalmeh <>
2021-06-07 21:50:08 +0000

    Small Changes - Kyle Did this

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,7 +1,7 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
-
+// added line
 // TODO 1.1a: Define candidateName // 
 let candidateName=" ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
